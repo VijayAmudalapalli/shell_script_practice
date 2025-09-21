d9eded8aabd270e6ea25abd3764ec57ca1ddd41d
gaddekaran <kumarkumar80130@gmail.com>
2025-09-21 16:29:24 +0530

    message

--- 05.ReadVariable.sh
+++ 05.ReadVariable.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+#1) This script will display the pin number while entering the pin
 echo "please enter your PIN number"
 read PIN_NUMBER
+
+#2) This script will not display the pin number while entering the pin
+echo "Please enter your PIN number"
+read -s PIN_NUMBER
