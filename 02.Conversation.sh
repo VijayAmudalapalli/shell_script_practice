07840b371dd5295deb6adf0dcf607668a069eb3b
gaddekaran <kumarkumar80130@gmail.com>
2025-09-21 14:27:48 +0530

    message

---
+++ 02.Conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Vijay :: Hi Sai, How are you ?"
+echo "Sai :: Hi Vijay, I am fine, How are you ?"
+echo "Vijay :: I am doing good, What are you learning recently ?"
+echo "Sai :: I am learning shell scripting, Vijay"
