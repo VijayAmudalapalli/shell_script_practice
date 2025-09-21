5600d0e94787a3e545f1ad6245263e231c5cce56
gaddekaran <kumarkumar80130@gmail.com>
2025-09-21 16:59:16 +0530

    message

--- 07.DateVariables.sh
+++ 07.DateVariables.sh
@@ -13,4 +13,6 @@ END_DATE=$(date +%s)
 
 TOTAL_TIME=$(($END_DATE - $START_DATE))
 
+echo "Start_Date in seconds : $START_DATE"
+echo "End_date in seconds : $END_DATE"
 echo "Script excuted in : $TOTAL_TIME seconds"
