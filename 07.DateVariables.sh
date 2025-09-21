94d3c0e8397d143602d3d865c3060d7a9a103ad4
gaddekaran <kumarkumar80130@gmail.com>
2025-09-21 16:56:48 +0530

    message

--- 07.DateVariables.sh
+++ 07.DateVariables.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-DATE=$(date)
+#!)
+#DATE=$(date)
 
-echo "TimeStamp excuted : $DATE"
+#echo "TimeStamp excuted : $DATE"
+
+
+#2)
+START_DATE=$(date +%s)
+sleep 5
+END_DATE=$(date +%s)
+
+TOTAL_TIME=$(($END_DATE - $START_DATE))
+
+echo "Script excuted in : $TOTAL_TIME seconds"
